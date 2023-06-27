3a628d7d274690b83df90842f5664eba6d168c7b
asylvz <asoylev@gmail.com>
2023-06-27 23:08:01 +0200

    Added extended WFA realign (still under development)

--- gaftools/cli/realign.py
+++ gaftools/cli/realign.py
@@ -11,16 +11,16 @@ from gaftools import __version__
 from gaftools.cli import log_memory_usage
 from gaftools.cli import CommandLineError
 from gaftools.timer import StageTimer
-from pywfa.align import WavefrontAligner
+from pywfa.align import (WavefrontAligner, cigartuples_to_str)
 from collections import namedtuple, defaultdict
 
 logger = logging.getLogger(__name__)
 
 
-def run(gaf, graph, fasta):
+def run(gaf, graph, fasta, extended):
     timers = StageTimer()
     
-    realign_gaf(gaf, graph, fasta)
+    realign_gaf(gaf, graph, fasta, extended)
     logger.info("\n== SUMMARY ==")
     total_time = timers.total()
     log_memory_usage()
@@ -45,6 +45,24 @@ class Edge:
         self.tags = tags
 """
 
+class Alignment:
+    def __init__(self, query_length, query_start, query_end, strand, path, path_length, path_start,
+                 path_end, residue_matches, cigar_length, score, cigar):
+        self.query_length = query_length
+        self.query_start = query_start
+        self.query_end = query_end
+        self.strand = strand
+        self.path = path
+        self.path_length = path_length
+        self.path_start = path_start
+        self.path_end = path_end
+        self.residue_matches = residue_matches
+        self.cigar_length = cigar_length
+        self.score = score
+        self.cigar = cigar
+        self.duplicate = False
+
+
 def parse_tag(s):
     name, type_id, value = s.split(':')
     assert len(name) == 2
@@ -54,6 +72,19 @@ def parse_tag(s):
         return name, value
     else:
         assert False
+
+
+def compare_aln(ln1, ln2):
+
+    if ln1.query_start < ln2.query_start:
+        return -1
+    elif ln1.query_start == ln2.query_start:
+        if ln1.query_end < ln2.query_end:
+            return 1
+        else:
+            return -1
+    else:
+        return 1
 
 
 def parse_gfa(gfa_filename, with_sequence=False):
@@ -112,64 +143,194 @@ def get_path(nodes, path):
     return ''.join(l)
 
 
-def wfa_alignment(gaf_line, ref, query):
 
-    match = 0
-    cigar_len = 0
+
+def overlap_ratio(x_start,x_end, y_start, y_end):
+
+    overlap = max(0, min(x_end, y_end) - max(x_start, y_start))
+    total_length = x_end - x_start + y_end - y_start
+    x_length = x_end - x_start
+    y_length = y_end - y_start
+
+    return max(2 * (overlap / total_length) , (overlap / x_length), (overlap/y_length))
+
+
+def filter_duplicates(aln):
+    import functools
+
+    #print("Filtering...")
+    for k in aln.keys():
+        aln[k].sort(key=functools.cmp_to_key(compare_aln))
+     
+    for read_name, mappings in aln.items():
+        if len(mappings) == 1:
+            continue
+        #print(read_name)
+        for cnt, line in enumerate(mappings):
+            if line.duplicate == True:
+                continue
+            #print("Query", line.query_start, line.query_end, line.score)
+            for cnt2, line2 in enumerate(mappings):
+                if cnt == cnt2 or line2.duplicate == True:
+                    continue
+                
+                sim = overlap_ratio(line.query_start, line.query_end, line2.query_start, line2.query_end)
+                
+                #print("-", line2.query_start, line2.query_end, line2.score, "sim =", sim) 
+                if sim > 0.8:
+                    if(line.score > line2.score):
+                        mappings[cnt2].duplicate = True
+                        #print("......Duplicate")
+                    else:
+                        mappings[cnt].duplicate = True
+                        #print("......Duplicate, breaking")
+                        break
+        """print()
+        for line in mappings:
+            print(line.query_start, line.query_end, line.duplicate)
+                    
+        print()"""
+
+
+def write_alignments(aln):
     
+    #Write the alignment back to the GAF
+    for read_name, mappings in aln.items():
+        for gaf_line in mappings:
+            if not gaf_line.duplicate:
+                sys.stdout.write("%s\t%d\t%d\t%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d" %(read_name, gaf_line.query_length, 
+                     gaf_line.query_start, gaf_line.query_end, gaf_line.strand, gaf_line.path, gaf_line.path_length, 
+                     gaf_line.path_start, gaf_line.path_end, gaf_line.residue_matches, gaf_line.cigar_length, 60))
+        
+                #if gaf_line.is_primary:
+                #    sys.stdout.write("\t%s" %gaf_line.is_primary[0])
+                sys.stdout.write("\tcg:Z:%s\n" %gaf_line.cigar)
+
+
+def wfa_alignment(aln, gaf_line, ref, query, path_start, extended):
+
     aligner = WavefrontAligner(ref)
-    res = aligner(query, clip_cigar=False)
+    if extended:
+        res = aligner(query, clip_cigar = True, min_aligned_bases_left = 30, min_aligned_bases_right = 30)
+    else:
+        res = aligner(query, clip_cigar = False)
     
-    #0 = match
+
+    match, mismatch, cigar_len, ins, deletion, soft_clip = 0, 0, 0, 0, 0, 0
+    cigar = ""
+    
     for op_type, op_len in res.cigartuples:
         if op_type == 0:
             match += op_len
+            cigar += str(op_len) + "="
+        elif op_type == 1:
+            ins += op_len
+            cigar += str(op_len) + "I"
+        elif op_type == 2:
+            deletion += op_len
+            cigar += str(op_len) + "D"
+        elif op_type == 4:
+            soft_clip += op_len
+        elif op_type == 8:
+            mismatch += op_len
+            cigar += str(op_len) + "X"
+        else:
+            print("ERRRORRR")
+            print(op_type, op_len)
         cigar_len += op_len
-        
-    cigar = aligner.cigarstring.replace("M", "=")
-        
-    #Write the alignment back to the GAF
-    sys.stdout.write("%s\t%d\t%d\t%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d" %(gaf_line.query_name, gaf_line.query_length, 
-                     gaf_line.query_start, gaf_line.query_end, gaf_line.strand, gaf_line.path, gaf_line.path_length, 
-                     gaf_line.path_start, gaf_line.path_end, match, cigar_len, gaf_line.mapping_quality))
-        
-    if gaf_line.is_primary:
-        sys.stdout.write("\t%s" %gaf_line.is_primary[0])
-    sys.stdout.write("\tcg:Z:%s\n" %cigar)
+    
+    """print("Match = ", match, " ins = ", ins, " del = ", deletion, " mismatch ", mismatch, " soft-clip = ", soft_clip)
+    print("Query check: ", match + ins + soft_clip + mismatch)
+    #print(cigartuples_to_str(res.cigartuples))
+    #print(res.cigartuples)
+    
+    print("Query start-end = ", res.text_start, res.text_end)
+    print("Reference start-end = ", res.pattern_start, res.pattern_end)
+    print("Score = ", res.score)
+    print("Cigar len = ", cigar_len, " Match = ", match)
+    #print(cigar)
+    print()
+    """
     
 
+    if extended:
+        if gaf_line.query_name in aln:
+            aln[gaf_line.query_name].append(Alignment(gaf_line.query_length, res.text_start,
+                                               res.text_end, gaf_line.strand, gaf_line.path,
+                                               gaf_line.path_length, path_start + res.pattern_start,
+                                               path_start + res.pattern_end, match, cigar_len,
+                                               res.score, cigar))
+        else:
+            aln[gaf_line.query_name] = [Alignment(gaf_line.query_length, res.text_start,
+                                               res.text_end, gaf_line.strand, gaf_line.path,
+                                               gaf_line.path_length, path_start + res.pattern_start,
+                                               path_start + res.pattern_end, match, cigar_len,
+                                               res.score, cigar)]
+    else:
+        cigar = aligner.cigarstring.replace("M", "=")
+
+        #Write the alignment back to the GAF
+        sys.stdout.write("%s\t%d\t%d\t%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d" %(gaf_line.query_name, gaf_line.query_length, 
+                     gaf_line.query_start, gaf_line.query_end, gaf_line.strand, gaf_line.path, gaf_line.path_length, 
+                     gaf_line.path_start, gaf_line.path_end, match, cigar_len, gaf_line.mapping_quality))
+
+        if gaf_line.is_primary:
+            sys.stdout.write("\t%s" %gaf_line.is_primary[0])
+        sys.stdout.write("\tcg:Z:%s\n" %cigar)
 
 
-def realign_gaf(gaf, graph, fasta):
+def realign_gaf(gaf, graph, fasta, extended):
     """
         Uses pyWFA (https://github.com/kcleal/pywfa)
     """
+    
 
     fastafile = pysam.FastaFile(fasta)
     nodes = parse_gfa(graph, with_sequence=True)
+    
+    #ref = "TCTTTACTCGCGCGTTGGAGAAATACAATAGT"
+    #query = "ACCCTCGCAAGCGTTGGAGAATG"
 
-
-    #gaf_lines = []
+    aln = {}
     for cnt, line in enumerate(parse_gaf(gaf)):
-        path_sequence = get_path(nodes, line.path)
-        ref = path_sequence[line.path_start:line.path_end]
-        query = fastafile.fetch(line.query_name, line.query_start, line.query_end)
-        wfa_alignment(line, ref, query)
+        if not line.is_primary:
+            continue
 
-        #if cnt == 1000:
-        #    print(cnt)
-        #    break
-        #gaf_lines.append(line)
-    
-    
-    """for line in gaf_lines:
         path_sequence = get_path(nodes, line.path)
-        ref = path_sequence[line.path_start:line.path_end]
-        query = fastafile.fetch(line.query_name, line.query_start, line.query_end)
 
-        wfa_alignment(line, ref, query)"""
+        if extended: 
+            extension_start = line.query_start
+            extension_end = line.query_length - line.query_end
+
+            path_start = line.path_start - extension_start
+            path_end = line.path_end + extension_end
+
+            if path_start < 0:
+                path_start = 0
+            if path_end > line.path_length:
+                path_end = line.path_length
+
+            ref = path_sequence[path_start:path_end]
+            query = fastafile.fetch(line.query_name)
+            wfa_alignment(aln, line, ref, query, path_start, extended)
+
+        else:
+            ref = path_sequence[line.path_start:line.path_end]
+            query = fastafile.fetch(line.query_name, line.query_start, line.query_end)
+            wfa_alignment([], line, ref, query, 0, False)
+
+        #if line.query_name == "bf9a7d6b-5595-4bf5-a48a-d93f28e195e1":
+        #if line.query_name == "beda39f4-2976-4c0b-9cb2-9bf4ebdf0129":
+
+
+        if cnt == 2000:
+            break
 
     fastafile.close()
+
+    if extended:
+        filter_duplicates(aln)
+        write_alignments(aln)
 
 
 # fmt: off
@@ -179,6 +340,7 @@ def add_arguments(parser):
     arg('gaf', metavar='GAF', help='GAF File')
     arg('graph', metavar='GFA', help='Input GFA file')
     arg('fasta', metavar='FASTA', help='Input FASTA file')
+    arg("--extended", action='store_true', help="Realign the whole read instead of the alignment only\'o.")
     #arg('-o', '--output', default=sys.stdout,
     #    help='Output GFA file. If omitted, use standard output.')
  
