f716aa87b38c8b5b6f6f627de69aca405f619f7f
Tobias Marschall <tobias.marschall@0ohm.net>
2023-09-04 16:59:28 +0200

    First test for gaftools realign

---
+++ tests/test_run_realign.py
@@ -0,0 +1,24 @@
+"""
+Tests for 'gaftools realign'
+"""
+
+from collections import namedtuple
+from gaftools.cli.realign import run_realign
+
+
+def test_order_gfa(tmp_path):
+    #gaftools realign alignments.gaf smallgraph.gfa reads.fa
+    input_gaf = 'tests/data/alignments.gaf'
+    output_gaf = str(tmp_path) + '/output.gaf'
+    run_realign(
+        gaf  = input_gaf,
+        graph = 'tests/data/smallgraph.gfa',
+        fasta = 'tests/data/reads.fa',
+        output = output_gaf,
+        ext=False
+    )
+
+    input_lines =  [l.split("\t") for l in open(input_gaf)]
+    output_lines = [l.split("\t") for l in open(output_gaf)]
+    assert len(input_lines) == len(output_lines)
+
