dbf9cfee5cd27e962aad5e54ad58d670e16132e8
Tobias Marschall <tobias.marschall@0ohm.net>
2023-09-04 17:20:25 +0200

    Test case for gaf.py

---
+++ tests/test_gaf.py
@@ -0,0 +1,28 @@
+"""
+Tests for gaf.py.
+"""
+
+from collections import namedtuple
+from gaftools.gaf import parse_gaf, GafLine
+
+
+def test_parse_gaf():
+    #gaftools realign alignments.gaf smallgraph.gfa reads.fa
+    gaf_lines = list(parse_gaf('tests/data/alignments.gaf'))
+    assert len(gaf_lines) == 2
+    for gaf_line in gaf_lines:
+        assert isinstance(gaf_line, GafLine)
+
+    assert gaf_lines[0].query_name == 'read_s8_s9'
+    assert gaf_lines[0].query_length == 398
+    assert gaf_lines[0].query_start == 0
+    assert gaf_lines[0].query_end == 398
+    assert gaf_lines[0].strand == '+'
+    assert gaf_lines[0].path == '>s8>s9'
+    assert gaf_lines[0].path_length == 10109
+    assert gaf_lines[0].path_start == 6166
+    assert gaf_lines[0].path_end == 6564
+    assert gaf_lines[0].residue_matches == 398
+    assert gaf_lines[0].alignment_block_length == 398
+    assert gaf_lines[0].mapping_quality == 60
+    assert gaf_lines[0].is_primary == False
