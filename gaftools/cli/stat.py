a171e88ab1aa15892343c970f198b0a6fd4184cf
Tobias Marschall <tobias.marschall@0ohm.net>
2023-09-05 11:03:48 +0200

    Add --output to stat command

--- gaftools/cli/stat.py
+++ gaftools/cli/stat.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 def run_stat(
     gaf_path,
     cigar_stat=False,
-    output=sys.stdout,
+    output=None,
 ):
     '''This function outputs some statistics of a GAF file. If you run with "--cigar" option, then
     cigar related statistics are also output. This increases the running time more than 10 folds
@@ -32,6 +32,11 @@ def run_stat(
     else:
         open_gaf = open
     
+    if output is None:
+        output = sys.stdout
+    else:
+        output = open(output, 'w')
+
     read_names = set()
     total_aligned_bases = 0
     total_mapq = 0
@@ -91,21 +96,19 @@ def run_stat(
                             total_match_large += 1
     
 
-    print("Total alignments:", alignment_count)
-    print("\tPrimary:", total_primary)
-    print("\tSecondary:", total_secondary)
-    print("Reads with at least one alignment:", len(read_names))
-    print("Total aligned bases:", str(total_aligned_bases))
-    print("Average mapping quality:", round((total_mapq/alignment_count), 1))
+    print("Total alignments:", alignment_count, file=output)
+    print("\tPrimary:", total_primary, file=output)
+    print("\tSecondary:", total_secondary, file=output)
+    print("Reads with at least one alignment:", len(read_names), file=output)
+    print("Total aligned bases:", str(total_aligned_bases), file=output)
+    print("Average mapping quality:", round((total_mapq/alignment_count), 1), file=output)
     
     if cigar_stat:
         print("Cigar string statistics:\n\tTotal deletion regions: %d (%d >50bps)\n\tTotal insertion regions: %d (%d >50bps)\n\tTotal substitution regions: %d (%d >50bps)\n\tTotal match regions: %d (%d >50bps)" % 
           (total_del, total_del_large, total_ins, total_ins_large, total_x, total_x_large,
-           total_match, total_match_large))
+           total_match, total_match_large), file=output)
         
-        print("Total perfect alignments (exact match):", total_perfect)
-    
-    print()
+        print("Total perfect alignments (exact match):", total_perfect, file=output)
 
     logger.info("\n== SUMMARY ==")
     total_time = timers.total()
@@ -122,6 +125,7 @@ def add_arguments(parser):
     arg = parser.add_argument
     # Positional arguments
     arg('gaf_path', metavar='GAF', help='Input GAF file')
+    arg('-o', '--output', default=None, help='Output file. If omitted, use standard output.')
     arg('--cigar', dest='cigar_stat', default=False, action='store_true', help='Outputs cigar related statistics (takes a lot long)')
 
 
