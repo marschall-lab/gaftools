3ee7c8184a36bfc661e17ad9f91c868ef841eb38
Fawaz Dabbaghie <fawaz.dabbaghieh@gmail.com>
2024-05-22 17:57:03 +0200

    removing the experimental extend functionality for now and changed the old strings for the alignment output to f strings

--- gaftools/cli/realign.py
+++ gaftools/cli/realign.py
@@ -5,6 +5,7 @@ Realign GAF file using wavefront alignment algorithm (WFA)
 import logging
 import sys
 import pysam
+import pdb
 import gaftools.gaf
 import multiprocessing as mp
 from gaftools import __version__
@@ -103,7 +104,63 @@ def write_alignments(aln, output):
                 # output.write("\tcg:Z:%s\n" %gaf_line.cigar)
 
 
-def wfa_alignment(aln, gaf_line, ref, query, path_start, extended, queue):
+def wfa_alignment(gaf_line, ref, query, queue):
+    """
+    Realigns the sequence using the wavefront alignment algorithm.
+    """
+    # if the sequence is too large, the running time and memory requirement increases too much
+    # Thus, we write the original alignment back to the GAF instead of realignment
+    if gaf_line.query_end - gaf_line.query_start < 60_000:
+        out_string = (f"{gaf_line.query_name}\t{gaf_line.query_length}\t{gaf_line.query_start}\t"
+                      f"{gaf_line.query_end}\t{gaf_line.strand}\t{gaf_line.path}\t{gaf_line.path_length}\t"
+                      f"{gaf_line.path_start}\t{gaf_line.path_end}\t{gaf_line.residue_matches}"
+                      f"\t{gaf_line.alignment_block_length}\t{gaf_line.mapping_quality}")
+        for k in gaf_line.tags.keys():
+            out_string += f"\t{k}{gaf_line.tags[k]}"
+        queue.put(out_string + "\n")
+
+    else:
+        aligner = WavefrontAligner(ref)
+        res = aligner(query, clip_cigar=False)
+
+        match, mismatch, cigar_len, ins, deletion, soft_clip = 0, 0, 0, 0, 0, 0
+        cigar = ""
+
+        for op_type, op_len in res.cigartuples:
+            if op_type == 0:
+                match += op_len
+                cigar += str(op_len) + "="
+            elif op_type == 1:
+                ins += op_len
+                cigar += str(op_len) + "I"
+            elif op_type == 2:
+                deletion += op_len
+                cigar += str(op_len) + "D"
+            elif op_type == 4:
+                soft_clip += op_len
+            elif op_type == 8:
+                mismatch += op_len
+                cigar += str(op_len) + "X"
+            else:
+                assert False
+            cigar_len += op_len
+
+        out_string = (f"{gaf_line.query_name}\t{gaf_line.query_length}\t{gaf_line.query_start}\t"
+                      f"{gaf_line.query_end}\t{gaf_line.strand}\t{gaf_line.path}\t{gaf_line.path_length}\t"
+                      f"{gaf_line.path_start}\t{gaf_line.path_end}\t{match}"
+                      f"\t{cigar_len}\t{gaf_line.mapping_quality}")
+
+        # queue.put(out_string + "\n")
+
+        for k in gaf_line.tags.keys():
+            # output.write("\t%s%s"%(k, gaf_line.tags[k]))
+            out_string += f"\t{k}{gaf_line.tags[k]}"
+            # out_string += "\t%s%s" % (k, gaf_line.tags[k])
+        queue.put(out_string + "\n")
+
+    queue.put(None)
+
+def wfa_alignment_old(aln, gaf_line, ref, query, path_start, extended, queue):
     # If the sequence is too large, the running time and memory requirement increases too much
     # Thus, we write the original alignment back to the GAF instead of realignment
     if gaf_line.query_end - gaf_line.query_start > 60000:
@@ -206,7 +263,9 @@ def wfa_alignment(aln, gaf_line, ref, query, path_start, extended, queue):
 def realign_gaf(gaf, graph, fasta, output, extended, cores=1):
     """
     Uses pyWFA (https://github.com/kcleal/pywfa)
-    parallelized on the level of alignment because in python cores
+    parallelized on the level of alignment
+
+    TODO: send a batch of reads to the wfa_alignment function instead of calling for each seq to limit function-calling overhead
     """
 
     fastafile = pysam.FastaFile(fasta)
@@ -242,19 +301,20 @@ def realign_gaf(gaf, graph, fasta, output, extended, cores=1):
 
             # making a process
             processes.append(
-                mp.Process(target=wfa_alignment, args=(aln, line, ref, query, path_start, extended, queue,)))
+                mp.Process(target=wfa_alignment_old, args=(aln, line, ref, query, path_start, output, extended, queue,)))
 
             # wfa_alignment(aln, line, ref, query, path_start, output, extended)
         else:
             ref = path_sequence[line.path_start:line.path_end]
             query = fastafile.fetch(line.query_name, line.query_start, line.query_end)
-            processes.append(mp.Process(target=wfa_alignment, args=([], line, ref, query, 0, False, queue,)))
+            processes.append(mp.Process(target=wfa_alignment, args=(line, ref, query, queue,)))
             # wfa_alignment([], line, ref, query, 0, output, False)
         if len(processes) == cores:
             # start running the processes and checking when they're done before starting the next batch
             for p in processes:
                 p.start()
             n_sentinals = 0
+
             while n_sentinals != len(processes):  # exits when all processes finished
                 out_string = queue.get()
                 if out_string is None:
@@ -303,7 +363,7 @@ def add_arguments(parser):
     arg('graph', metavar='GFA', help='Input GFA file')
     arg('fasta', metavar='FASTA', help='Input FASTA file')
     arg('-o', '--output', default=None, help='Output file. If omitted, use standard output.')
-    arg("--ext", action='store_true', help="Extend the aligned path (use for Minigraph alignments).")
+    # arg("--ext", action='store_true', help="Extend the aligned path (use for Minigraph alignments).")
     arg("-c", "--cores", metavar="CORES", default=1, type=int, help="Number of cores to use for alignments.")
 
 
