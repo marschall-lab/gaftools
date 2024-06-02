1b623361017fcf698411dca7e9e15e119cacb8fd
Fawaz Dabbaghie <fawaz.dabbaghieh@gmail.com>
2024-06-02 17:22:25 +0200

    multiprocess realign been tested and seems to work, there are some small edits here and there

--- gaftools/utils.py
+++ gaftools/utils.py
@@ -1,4 +1,7 @@
 import re
+import tracemalloc
+import linecache
+
 
 complement = str.maketrans('ACGT', 'TGCA')
 tag_regex = r"^[A-Za-z][A-Za-z][:][AifZHB][:][ !-~]*$"
@@ -70,3 +73,29 @@ def is_unstable(line):
         return False
     
     return True
+
+
+def display_top(snapshot, key_type='lineno', limit=3):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = "/".join(frame.filename.split("/")[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
