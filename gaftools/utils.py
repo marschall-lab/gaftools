99607d5aa3314db1c840adb0f5fffe6d699764fc
Samarendra <samarendrapani1998@gmail.com>
2025-03-17 13:01:30 +0100

    moving set of default chromosomes.

--- gaftools/utils.py
+++ gaftools/utils.py
@@ -2,6 +2,34 @@ import re
 import tracemalloc
 import linecache
 
+DEFAULT_CHROMOSOME = [
+    "chr1",
+    "chr2",
+    "chr3",
+    "chr4",
+    "chr5",
+    "chr6",
+    "chr7",
+    "chr8",
+    "chr9",
+    "chr10",
+    "chr11",
+    "chr12",
+    "chr13",
+    "chr14",
+    "chr15",
+    "chr16",
+    "chr17",
+    "chr18",
+    "chr19",
+    "chr20",
+    "chr21",
+    "chr22",
+    "chrX",
+    "chrY",
+    "chrM",
+]
+
 complement = str.maketrans("ACGT", "TGCA")
 tag_regex = r"^[A-Za-z][A-Za-z][:][AifZHB][:][ !-~]*$"
 
