8d07e9dc6e93b331361be0489558b8e94581c9d6
Samarendra <samarendrapani1998@gmail.com>
2026-04-13 11:05:31 +0200

    more testing for find_path

--- tests/test_find_path.py
+++ tests/test_find_path.py
@@ -3,6 +3,8 @@ Test for gaftools find_path
 """
 
 from gaftools.cli.find_path import run
+from gaftools.cli.order_gfa import run_order_gfa
+import pytest
 
 
 def parse_output(filename):
@@ -12,15 +14,84 @@ def parse_output(filename):
     return [parse_line(l) for l in open(filename)]
 
 
-# gfa_path, path=None, paths_file=None, keep_going=True, output=None, fasta=False
-# testing for a file input for path list
-def test_file_input(tmp_path):
+def test_smallgraph(tmp_path):
     input_gfa = "tests/data/smallgraph.gfa"
-    input_path_list = "tests/data/find_path-input.txt"
+    input_path_list = "tests/data/find_path/smallgraph-input.txt"
     output = str(tmp_path) + "/output.fasta"
-    truth = "tests/data/find_path-output.fasta"
+    truth = "tests/data/find_path/smallgraph-output.fasta"
     run(
         input_gfa, path=None, paths_file=input_path_list, keep_going=True, output=output, fasta=True
+    )
+    output_lines = parse_output(output)
+    truth_lines = parse_output(truth)
+    for n in range(len(output_lines)):
+        assert output_lines[n] == truth_lines[n]
+
+
+@pytest.mark.parametrize(
+    "gfa_file",
+    [
+        "graph.gfa",
+        "graph.gfa.gz",
+        "reference-graph.gfa",
+    ],
+)
+def test_customgraph(tmp_path, gfa_file):
+    input_gfa = f"tests/data/gfa2rgfa/{gfa_file}"
+    input_path_list = "tests/data/find_path/customgraph-input.txt"
+    output = str(tmp_path) + "/output.fasta"
+    truth = "tests/data/find_path/customgraph-output.fasta"
+    run(
+        input_gfa, path=None, paths_file=input_path_list, keep_going=True, output=output, fasta=True
+    )
+    output_lines = parse_output(output)
+    truth_lines = parse_output(truth)
+    for n in range(len(output_lines)):
+        assert output_lines[n] == truth_lines[n]
+
+
+@pytest.mark.parametrize("gfa_file", ["smallgraph-ordered.gfa", "customgraph-ordered.gfa"])
+def test_orderedgraph1(tmp_path, gfa_file):
+    input_gfa = f"tests/data/order_gfa/{gfa_file}"
+    input_path_list = (
+        "tests/data/find_path/customgraph-input.txt"
+        if "custom" in gfa_file
+        else "tests/data/find_path/smallgraph-input.txt"
+    )
+    output = str(tmp_path) + "/output.fasta"
+    truth = (
+        "tests/data/find_path/customgraph-output.fasta"
+        if "custom" in gfa_file
+        else "tests/data/find_path/smallgraph-output.fasta"
+    )
+    run(
+        input_gfa, path=None, paths_file=input_path_list, keep_going=True, output=output, fasta=True
+    )
+    output_lines = parse_output(output)
+    truth_lines = parse_output(truth)
+    for n in range(len(output_lines)):
+        assert output_lines[n] == truth_lines[n]
+
+
+def test_orderedgraph2(tmp_path):
+    input_gfa = "tests/data/gfa2rgfa/reference-graph.gfa"
+    run_order_gfa(
+        gfa_filename=input_gfa,
+        outdir=str(tmp_path),
+        by_chrom=False,
+        without_sequence=True,
+    )
+    ordered_gfa = str(tmp_path) + "/reference-graph-complete.gfa"
+    input_path_list = "tests/data/find_path/customgraph-input.txt"
+    output = str(tmp_path) + "/output.fasta"
+    truth = "tests/data/find_path/customgraph-output.fasta"
+    run(
+        ordered_gfa,
+        path=None,
+        paths_file=input_path_list,
+        keep_going=True,
+        output=output,
+        fasta=True,
     )
     output_lines = parse_output(output)
     truth_lines = parse_output(truth)
